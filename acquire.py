ca4e8ce7237bd764716925bc18f401d36c183ea9
jamesallen0351 <james.allen0351@gmail.com>
2021-12-19 08:31:51 -0600

    add function to acquire.py

--- acquire.py
+++ acquire.py
@@ -1,4 +1,29 @@
 # craft beer acquire
 
-def get_beer():
+# imports
+
+import pandas as pd
+
+# acquire function
+
+def beer_df():
+    '''
+    This function takes in the beer data and brewery data 
+    and merges into one dataframe 
+    and renames the columns for readbility
+    and saves the new dataframe as a csv
+    '''
+    # getting the beers data
+    df = pd.read_csv('beers.csv')
+    # getting the breweries data
+    df_1 = pd.read_csv('breweries.csv')
+    # merging the two csv files together
+    df = pd.merge(df, df_1, 
+                   on='Unnamed: 0', 
+                   how='inner')
+    # renaming columns for readability
+    df = df.rename(columns = {"style": "beer_style", "name_y": "brewery", "Unnamed: 0": "number", "name_x": "beer"})
     
+    df.to_csv('beer_data.csv')
+    
+    return df
