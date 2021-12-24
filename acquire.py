dad08a8f41cc5071f2cc794b056c73511557abdd
jamesallen0351 <james.allen0351@gmail.com>
2021-12-24 07:16:22 -0600

    add split and model df

--- acquire.py
+++ acquire.py
@@ -57,3 +57,22 @@ def top_beers():
     top_beer = df.beer_style.value_counts().rename_axis('beer_type').reset_index(name='total')
     
     return top_beer
+
+from sklearn.model_selection import train_test_split
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
